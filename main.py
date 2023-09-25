ba2e35e286d9973cf8b4381f4e45241f06868da0
Dr-Passmore <phillipspassmore@gmail.com>
2023-09-25 17:46:25 +0100

    logging added

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-
+import logging
 
 import youtube_video_download
 
 class LoadingScript:
     def __init__ (self):
-        print("test")
+        logging('Initising Youtube Download App')
         youtube_video_download.VideoDownload()
 
-    
+logging.basicConfig(filename='YouTubeVideoDownloader.log', 
+                    filemode='a', 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == "__main__":
--- youtube_video_download.py
+++ youtube_video_download.py
@@ -1,11 +1,16 @@
 import pytube
+import logging
 
 class VideoDownload:
     def __init__ (self):
-        print("test")
+        logging('VideoDownload')
 
     
-
+logging.basicConfig(filename='YouTubeVideoDownloader.log', 
+                    filemode='a', 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 if __name__ == "__main__":
     VideoDownload()
