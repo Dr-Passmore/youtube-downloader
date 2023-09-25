e1d8e3f80ce0ff5deb377ba3f2b047357f83527b
Dr-Passmore <phillipspassmore@gmail.com>
2023-09-25 19:06:24 +0100

    File structure updated

--- application_user_interface.py
+++ application_user_interface.py
@@ -1,13 +1,8 @@
 import logging
 
-import youtube_video_download
-import application_user_interface
-
-class LoadingScript:
-    def __init__ (self):
-        logging.info('Initising Youtube Download App')
-        youtube_video_download.VideoDownload()
-        application_user_interface.UserInterface()
+class UserInterface:
+    def __init__(self) -> None:
+        pass
 
 logging.basicConfig(filename='YouTubeVideoDownloader.log', 
                     filemode='a', 
@@ -17,4 +12,4 @@ logging.basicConfig(filename='YouTubeVideoDownloader.log',
 
 
 if __name__ == "__main__":
-    LoadingScript()
+    UserInterface()
