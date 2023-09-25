8a6c39f7abc462d3e2cb948dff06731cfbe837ea
Dr-Passmore <phillipspassmore@gmail.com>
2023-09-25 16:15:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+import youtube_video_download
+
+class LoadingScript:
+    def __init__ (self):
+        print("test")
+        youtube_video_download.VideoDownload()
+
+    
+
+
+if __name__ == "__main__":
+    LoadingScript()
---
+++ youtube_video_download.py
@@ -0,0 +1,11 @@
+import pytube
+
+class VideoDownload:
+    def __init__ (self):
+        print("test")
+
+    
+
+
+if __name__ == "__main__":
+    VideoDownload()
