0082262645ce6f9ad602d5fae18ae208081ebe53
Phillip Passmore <phillipspassmore@gmail.com>
2023-10-11 22:50:36 +0100

    calling log info from UI
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- application_user_interface.py
+++ application_user_interface.py
@@ -7,11 +7,13 @@ class UserInterface:
     def __init__(self) -> None:
         self.window = tk.Tk()
         self.window.title("YouTube Video Downloader")
-        self.window.geometry("500x500") 
+        self.window.geometry("800x500") 
         
-
+        UserInterface.create_widgets(self)
+        self.log_display()
         UserInterface.create_window(self)
 
+        
     def create_window(self):
         self.window.mainloop()
         
@@ -27,6 +29,17 @@ class UserInterface:
     def setup_window_close_event(self):
         self.window.protocol("WM_DELETE_WINDOW", self.confirm_exit)
 
+    def create_widgets(self):
+        # Exit confirmation checks for close event
+        self.setup_window_close_event()
+        
+        # Exit button added
+        exit_button = tk.Button(self.window, text="Exit", command=self.confirm_exit)
+        exit_button.pack(side=tk.RIGHT, anchor=tk.SE, padx=10, pady=10)
+        
+        self.log_text = tk.Text(self.window, height=10, state=tk.DISABLED)
+        self.log_text.pack(side=tk.BOTTOM, anchor=tk.S, fill=tk.X, padx=20, pady=20)
+
     def url_input():
         pass
     
@@ -38,6 +51,23 @@ class UserInterface:
 
     def download_button():
         pass
+    
+    def log_display(self):
+        log_messages = self.get_last_5_log_messages()
+        self.log_text.config(state=tk.NORMAL)
+        self.log_text.delete(1.0, tk.END)  # Clear previous log messages
+        for message in log_messages:
+            self.log_text.insert(tk.END, message)
+        self.log_text.config(state=tk.DISABLED)
+        
+    def get_last_5_log_messages(self):
+        # Function to retrieve the last 5 log messages from the log file
+        log_messages = []
+        with open('YouTubeVideoDownloader.log', 'r') as log_file:
+            lines = log_file.readlines()
+            log_messages = lines[-5:]
+        return log_messages
+
 
 logging.basicConfig(filename='YouTubeVideoDownloader.log', 
                     filemode='a', 
